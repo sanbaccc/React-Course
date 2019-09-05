d0e2b15dfab8aa75bef5db942871337de5e2ee3a
Yanchun <yanchun@avarobotics.com>
2019-09-05 14:29:00 -0400

    Passing props, Comment Cards

--- components/src/index.js
+++ components/src/index.js
@@ -1,9 +1,48 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import Comment from "./comment"
+import faker from "faker"
+import ApprovalCard from "./ApprovalCard"
 
 const App = () => {
     return (
-        <div> Hello </div>
+        <div className="ui container comments">
+            <ApprovalCard>
+                <Comment
+                    author="aloha"
+                    timeAgo="10 min"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <Comment
+                    author="ming"
+                    timeAgo="4 min"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <Comment
+                    author="share"
+                    timeAgo="1 min"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <Comment
+                    author="wuka"
+                    timeAgo="28 min"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <Comment
+                    author="Dehor"
+                    timeAgo="16 min"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+        </div>
     );
 };
 
