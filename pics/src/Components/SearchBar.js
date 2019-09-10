5184929d4349a3b372910d929ae94fe1456d3c89
Yanchun <yanchun@avarobotics.com>
2019-09-10 00:12:26 -0400

    pics, call back, confused

---
+++ pics/src/Components/SearchBar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+    state = { term: "" };
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
+    onInputClick() {
+        console.log("input was clicked");
+    }
+
+    onFormSubmit = e => {
+        e.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
+
+    render() {
+        return (
+            <div className="ui segment" >
+                <form onSubmit={this.onFormSubmit} className="ui form" >
+                    <div className="field" >
+                        <label> Image Search </label>
+                        <input type="text" onChange={e => this.setState({ term: e.target.value })}
+                            value = {this.state.term} />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
